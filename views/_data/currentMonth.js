86b5a761d72fddf33d1da2b99c328b443f82f08e
YH <yhabayeb@gmail.com>
2025-01-27 12:50:12 +0100

    Change currentMonth back to js, because Eleventy doesnt support data from typescript

--- views/_data/currentMonth.js
+++ views/_data/currentMonth.js
@@ -1,13 +1,6 @@
 import months from "./months.json" with { type: "json" };
 
-type Month = {
-  name: string;
-  abbreviation: string;
-  vegetables: string[];
-  fruit: string[];
-}
-
-export default (): Month => {
+export default () => {
   const currentMonthIndex = new Date().getMonth();
   return months[currentMonthIndex];
 };
